f7146469f83949435ecc3c95f5e79ac906987097
williamsmichael <willia32@hotmail.com>
2015-10-08 01:20:38 -0700

    admin set for SignUp class

--- trydjango/newsletter/admin.py
+++ trydjango/newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ('email', 'timestamp', 'updated')
+
+admin.site.register(SignUp, SignUpAdmin)
